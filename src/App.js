d234449cfd516715abb6fe14f3bec9e4d6b9fadd
Ryan Lewis <rlewis94@gmail.com>
2021-07-28 23:18:29 -0700

    refactoring to jss

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import logo from './logo.svg';
-import './App.css';
+import {createUseStyles} from 'react-jss';
+
+const useStyles = createUseStyles({
+  main: {
+    textAlign: 'center'
+  },
+  header: {
+    backgroundColor: '#535D8D',
+    display: 'flex',
+    '& div': {
+      margin: 10,
+      flexGrow: 1,
+      flexShrink: 1,
+      flexBasis: 0
+    },
+  },
+  links: {
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    '> a': {
+      padding: '1em'
+    },
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={classes.main}>
+      <header className={classes.header}>
         <div>
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} alt="logo" />
         </div>
-        <div className="links">
+        <div className={classes.links}>
           <a href="./about">About Us</a>
           <a href="./capabilities">Capabilities</a>
           <a href="./contact">Contact</a>

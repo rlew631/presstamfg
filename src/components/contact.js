15d9c9032ec2dceffa7d80aa4deefcf08da1dd27
Ryan Lewis <rlewis94@gmail.com>
2021-08-03 22:47:38 -0700

    properly bottom aligned the contact info

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,21 @@
+import {createUseStyles} from 'react-jss';
 import { Container, Row, Col } from "reactstrap";
 import copy from '../copy.json';
 
+const useStyles = createUseStyles({
+  info: {
+    "align-self": "flex-end"
+  }
+});
+
+
 function Contact() {
+  const classes = useStyles();
   return(
     <div>
       <Container>
-        <Row>
-          <Col>
+        <Row className={classes.info}>
+          <Col lg={{size: 5, offset: 1}}>
             <div align="left">
               Email: {copy.Contact.Email}
               <br></br>
@@ -15,8 +24,13 @@ function Contact() {
               Address: {copy.Contact.Address}
             </div>
           </Col>
-          <Col>
+          <Col lg="5">
             <h1>Put in an inquiry form here</h1>
+            line one <br></br>
+            line two <br></br>
+            line three <br></br>
+            line four <br></br>
+            line five <br></br>
           </Col>
         </Row>
       </Container>

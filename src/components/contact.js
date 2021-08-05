f27ab7d0847a09765f44f3e290044006012cf967
Ryan Lewis <rlewis94@gmail.com>
2021-08-04 18:18:13 -0700

    cleaned up form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {createUseStyles} from 'react-jss';
-import { Container, Row, Col } from "reactstrap";
+import {Button, Container, Row, Col } from "reactstrap";
 import copy from '../copy.json';
 
 const useStyles = createUseStyles({
@@ -28,93 +28,77 @@ class ContactForm extends React.Component {
     const { status } = this.state;
     return (
       <section className="paralax-mf footer-paralax bg-image sect-mt4 route">
-        <Col sm="12">
-          <div className="contact-mf">
-            <div id="contact" className="box-shadow-full">
-              <div className="row">
-                <div className="col-md-7">
-                  <div className="title-box-2">
-                    <h2 className="title-left">Send A Message</h2>
-                  </div>
-                  <div>
-                    <form
-                      action="https://formspree.io/f/xvodwvwz"
-                      method="POST"
-                      className="contactForm"
-                      onSubmit={this.submitForm}
-                    >
-                      <div id="sendmessage">
-                        Your message has been sent. Thank you!
-                      </div>
-                      <div id="errormessage"></div>
-                      <div className="row">
-                        <div className="col-md-12 mb-3">
-                          <div className="form-group">
-                            <input
-                              type="text"
-                              name="name"
-                              className="form-control"
-                              id="name"
-                              placeholder="Your Name"
-                              data-rule="minlen:4"
-                              data-msg="Please enter at least 4 chars"
-                            />
-                            <div className="validation"></div>
-                          </div>
-                        </div>
-                        <div className="col-md-12 mb-3">
-                          <div className="form-group">
-                            <input
-                              type="email"
-                              className="form-control"
-                              name="_replyto"
-                              id="email"
-                              placeholder="Your Email"
-                              data-rule="email"
-                              data-msg="Please enter a valid email"
-                            />
-                            <div className="validation"></div>
-                          </div>
-                        </div>
-                        <div className="col-md-12 mb-3">
-                          <div className="form-group">
-                            <input
-                              type="text"
-                              className="form-control"
-                              name="_subject"
-                              id="subject"
-                              placeholder="Subject"
-                              data-rule="minlen:4"
-                              data-msg="Please enter at least 8 chars"
-                            />
-                            <div className="validation"></div>
-                          </div>
-                        </div>
-                        <div className="col-md-12 mb-3">
-                          <div className="form-group">
-                            <textarea
-                              className="form-control"
-                              name="message"
-                              rows="5"
-                              data-rule="required"
-                              data-msg="Please write something for us"
-                              placeholder="Message"
-                            ></textarea>
-                            <div className="validation"></div>
-                          </div>
-                        </div>
-                        <div className="col-md-12">
-                          {status === "SUCCESS" ? <button type="submit" className="button button-a button-big button-rouded">Your message has been sent!</button> : <button type="submitted" className="button button-a button-big button-rouded">Send Message</button>}
-                          {status === "ERROR" && <button type="failed" className="button button-a button-big button-rouded">There was an error, please contact me on LinkedIn</button>}
-                        </div>
-                      </div>
-                    </form>
-                  </div>
+        <h2 className="title-left">Send A Message</h2>
+        <div>
+          <form
+            action="https://formspree.io/f/xvodwvwz"
+            method="POST"
+            className="contactForm"
+            onSubmit={this.submitForm}
+          >
+            <Row>
+              <div className="col-md-12 mb-3">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    id="name"
+                    placeholder="Your Name"
+                    data-rule="minlen:4"
+                    data-msg="Please enter at least 4 chars"
+                  />
+                  <div className="validation"></div>
                 </div>
               </div>
-            </div>
-          </div>
-        </Col>
+              <div className="col-md-12 mb-3">
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="_replyto"
+                    id="email"
+                    placeholder="Your Email"
+                    data-rule="email"
+                    data-msg="Please enter a valid email"
+                  />
+                  <div className="validation"></div>
+                </div>
+              </div>
+              <div className="col-md-12 mb-3">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="_subject"
+                    id="subject"
+                    placeholder="Subject"
+                    data-rule="minlen:4"
+                    data-msg="Please enter at least 8 chars"
+                  />
+                  <div className="validation"></div>
+                </div>
+              </div>
+              <div className="col-md-12 mb-3">
+                <div className="form-group">
+                  <textarea
+                    className="form-control"
+                    name="message"
+                    rows="5"
+                    data-rule="required"
+                    data-msg="Please write something for us"
+                    placeholder="Message"
+                  ></textarea>
+                  <div className="validation"></div>
+                </div>
+              </div>
+              <Col md="12">
+                {status === "SUCCESS" ? <Button type="submit" className="button button-a button-big button-rouded">Your message has been sent!</Button> : <Button type="submitted" className="button button-a button-big button-rouded">Send Message</Button>}
+                {status === "ERROR" && <Button type="failed" className="button button-a button-big button-rouded">There was an error, please contact us at Shop@getpressta.com</Button>}
+              </Col>
+            </Row>
+          </form>
+        </div>
       </section>
     );
   }

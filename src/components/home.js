18dfd398548db8fb88a8f0947003b6c66c3169fe
Ryan Lewis <rlewis94@gmail.com>
2021-08-06 13:22:12 -0700

    cleaned up front page more

--- src/components/home.js
+++ src/components/home.js
@@ -14,71 +14,44 @@ const useStyles = createUseStyles({
 function Home() {
   const classes = useStyles();
   return (
-    <div>
-      <Container>
-        <Row justify="center">
-          <Col lg={{size: 10, offset: 1}}>
-            <h1>What We Do</h1>
-            <p className = {classes.statement}>
-              {copy.Home.WhatWeDoStatement}
-            </p>
-          </Col>
-          <Col lg={{size: 10, offset: 1}}>
-            <h1>About The Team</h1>
-            <p className = {classes.statement}>
-              {copy.Home.AboutUsStatement}
-            </p>
-          </Col>
-          <Col lg={{size: 10, offset: 1}}>
-            <h1>Why Use Pressta</h1>
-            <p className = {classes.statement}>
-              {copy.Home.WhyUsePresstaStatement} 
-            </p>
-          </Col>
-          <Col lg={{size: 10, offset: 1}}>
-            <h1>Industries Served</h1>
-            <p className = {classes.statement}>
-              {copy.Home.IndustriesStatement}
-            </p>
-          </Col>
-          <Col lg={{size: 10, offset: 1}}>
-            <h1>Classifications and Certifications</h1>
-            <p className = {classes.statement}>
-              {copy.Home.ClassificationsCertifications}
-              <br></br><br></br>
-              We are registered on SAM.gov as Fresh Press LLC and DUNS / CAGE codes are 118160742 and 92SM7 respecively.
-              <br></br><br></br>
-              Pressta has been in business since 2017.
-            </p>
-          </Col>
-        </Row>
-      </Container>
-
-      {/* <Container>
-        <br></br><br></br><br></br><br></br>
-        <h2>Services (might get rid of this section)</h2>
-        <Row justify="center">
-          <Col lg={{size: 3, offset: 1.5}} md={{size: 10, offset:1}}>
-            <h3>Design</h3>
-            <div>
-              {copy.Home.Services.Design}
-            </div>
-          </Col>
-          <Col lg="3" md={{size:10, offset:1}}>
-            <h3>Engineering</h3>
-            <div>
-              {copy.Home.Services.Engineering}
-            </div>
-          </Col>
-          <Col lg="3" md={{size:10, offset:1}}>
-            <h3>Manufacturing</h3>
-            <div>
-              {copy.Home.Services.Manufacturing}
-            </div>
-          </Col>
-        </Row>
-      </Container> */}
-    </div>
+    <Container>
+      <Row justify="center">
+        <Col lg={{size: 10, offset: 1}}>
+          <h1>What We Do</h1>
+          <p className = {classes.statement}>
+            {copy.Home.WhatWeDoStatement}
+          </p>
+        </Col>
+        <Col lg={{size: 10, offset: 1}}>
+          <h1>About The Team</h1>
+          <p className = {classes.statement}>
+            {copy.Home.AboutUsStatement}
+          </p>
+        </Col>
+        <Col lg={{size: 10, offset: 1}}>
+          <h1>Why Use Pressta</h1>
+          <p className = {classes.statement}>
+            {copy.Home.WhyUsePresstaStatement} 
+          </p>
+        </Col>
+        <Col lg={{size: 10, offset: 1}}>
+          <h1>Industries Served</h1>
+          <p className = {classes.statement}>
+            {copy.Home.IndustriesStatement}
+          </p>
+        </Col>
+        <Col lg={{size: 10, offset: 1}}>
+          <h1>Classifications and Certifications</h1>
+          <p className = {classes.statement}>
+            {copy.Home.ClassificationsCertifications}
+            <br></br><br></br>
+            We are registered on SAM.gov as Fresh Press LLC and DUNS / CAGE codes are 118160742 and 92SM7 respecively.
+            <br></br><br></br>
+            Pressta has been in business since 2017.
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
